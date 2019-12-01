b92a852042e0a09db5cf20f3125f3f42a3888617
israel <israel.bejarano.a@gmail.com>
2019-12-01 13:48:35 +0100

    mostrar en las respuestas el puerto para ver el balanceo de carga

--- springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/controllers/ProductoController.java
@@ -2,8 +2,10 @@
 package com.microservicios.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +17,25 @@ import com.microservicios.springboot.app.productos.models.service.IProductoServi
 public class ProductoController {
 	
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private IProductoService productoService;
 	
 	@GetMapping("/listar")
 	public List<Producto> listar() {
-		return productoService.findAll();
+		return productoService.findAll().stream().map(producto -> {
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			return producto;
+		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		return productoService.findById(id);
+		
+		Producto producto = productoService.findById(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return producto;
 	}
 
 }
