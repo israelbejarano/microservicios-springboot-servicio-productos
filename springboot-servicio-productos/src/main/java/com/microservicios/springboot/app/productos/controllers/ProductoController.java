64c7e03a8d0f8ece397315d5aeddd8abdfe147bc
israel <israel.bejarano.a@gmail.com>
2019-12-01 13:59:20 +0100

    otra forma de obtener el puerto de configuracion

--- springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/controllers/ProductoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,11 @@ import com.microservicios.springboot.app.productos.models.service.IProductoServi
 @RestController
 public class ProductoController {
 	
-	@Autowired
-	private Environment env;
+	// @Autowired
+	// private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
 	
 	@Autowired
 	private IProductoService productoService;
@@ -25,7 +29,8 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> listar() {
 		return productoService.findAll().stream().map(producto -> {
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -34,7 +39,8 @@ public class ProductoController {
 	public Producto detalle(@PathVariable Long id) {
 		
 		Producto producto = productoService.findById(id);
-		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
 		return producto;
 	}
 
